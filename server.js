27415a2ca3c73a3f103e5a5a233d24776288fe4b
Zhaoyan <irisfeng0321@outlook.com>
2022-04-10 21:50:46 -0400

    a9 connect mongoDB

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import express from 'express';
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
-import mongoose from "mongoose";
 
+const mongoose = require('mongoose');
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
                           || 'mongodb://localhost:27017/webdev'
 mongoose.connect(CONNECTION_STRING);
@@ -19,5 +19,6 @@ app.use(express.json());
 helloController(app);
 userController(app);
 tuitsController(app);
+app.get('/', (req,res) => res.send('Welcome'))
 
 app.listen(process.env.PORT || 4000);
